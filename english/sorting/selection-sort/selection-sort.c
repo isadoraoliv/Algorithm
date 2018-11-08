5b168d5406c6edb3bd2011cd3cfa6080b2bcb75e
Isadora Oliveira <oliveiraisadora119@gmail.com>
2018-11-07 23:53:10 -0200

    code main

--- english/sorting/selection-sort/selection-sort.c
+++ english/sorting/selection-sort/selection-sort.c
@@ -1,17 +1,35 @@
+#include <stdio.h> 
+
+int i,j;
+
 
 
 void selectionSort(int array[], int n){
-
+  
+  int menor, tmp;
+  
   for(i = 0; i < (n-1); i++){
-    int menor = i;
-    for(j = (i + 1); j < n; j++){
-      if(array[menor] > array[j])
-      menor = j;
-      printf("%d\n",menor);
+    menor = i;
+      for(j = (i + 1); j < n; j++){
+        if(array[menor] > array[j])
+          menor = j;
+          printf("%d\n",menor);
     }
     if(array[i] != array[menor])
       tmp = array[menor];
       array[menor] = array[i];
       array[i] = tmp;
   }
+}
+
+
+
+
+
+int main( ){
+
+    int array[] = {103,112,35,62,40,20};
+    int n = sizeof(array)/sizeof(array[0]);
+    selectionSort(array, n);
+    mostrar(array,n);
 }
