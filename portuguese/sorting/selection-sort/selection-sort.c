42a180cd54973be7e9e6b4e4e7c15d452b164c32
Isadora Oliveira <oliveiraisadora119@gmail.com>
2018-11-07 23:50:53 -0200

    code sorting selection

--- portuguese/sorting/selection-sort/selection-sort.c
+++ portuguese/sorting/selection-sort/selection-sort.c
@@ -1,1 +1,15 @@
-# Selection Sort
+void selectionSort(int array[], int n){
+
+  for(i = 0; i < (n-1); i++){
+    int menor = i;
+    for(j = (i + 1); j < n; j++){
+      if(array[menor] > array[j])
+      menor = j;
+      printf("%d\n",menor);
+    }
+    if(array[i] != array[menor])
+      tmp = array[menor];
+      array[menor] = array[i];
+      array[i] = tmp;
+  }
+}
