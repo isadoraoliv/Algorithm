97367c07139c23587ac3d41201941af7db0c96e3
Isadora Oliveira <oliveiraisadora119@gmail.com>
2018-11-11 23:48:55 -0200

    add code selection sort

--- portuguese/sorting/selection-sort/selection-sort.c
+++ portuguese/sorting/selection-sort/selection-sort.c
@@ -1,15 +1,34 @@
-void selectionSort(int array[], int n){
 
+
+#include <stdio.h> 
+
+int i,j;
+
+void selectionSort(int array[], int n){
+  
+  int menor, tmp;
+  
+  //buscar o menor elemento em relação a "i"
   for(i = 0; i < (n-1); i++){
-    int menor = i;
-    for(j = (i + 1); j < n; j++){
-      if(array[menor] > array[j])
-      menor = j;
-      printf("%d\n",menor);
+    menor = i;
+      for(j = (i + 1); j < n; j++){
+        if(array[menor] > array[j])
+          menor = j;
     }
+    
+    //alterar os valores da posição atual com o menor
     if(array[i] != array[menor])
       tmp = array[menor];
       array[menor] = array[i];
       array[i] = tmp;
   }
 }
+
+int main( ){
+  
+    int array[] = {103,112,35,62,40,20};
+    int n = sizeof(array)/sizeof(array[0]);
+  
+    selectionSort(array, n);
+}
+
