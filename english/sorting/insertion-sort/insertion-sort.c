9aa80d32b8a5779bdd99b48143393bec1c850e83
Isadora Oliveira <oliveiraisadora119@gmail.com>
2018-11-29 19:42:50 -0200

    added insertion sort code

--- english/sorting/insertion-sort/insertion-sort.c
+++ english/sorting/insertion-sort/insertion-sort.c
@@ -1,1 +1,20 @@
-insertion
+
+#include <stdio.h>
+
+int i,j;
+
+void insertionSort(int array[], int n) {
+	
+  int tmp;
+
+  for (i = 1; i < n; i++) {
+    tmp = array[i];
+    j = i - 1;
+
+    while ((j >= 0) && (array[j] > tmp)) {
+      array[j + 1] = array[j];
+        j--;
+    }
+    array[j + 1] = tmp;
+   }
+}
